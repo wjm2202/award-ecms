64732dde752d865d8efcfe065f91b4dbad5071e2
Jayce-C <Blackswankk@gmail.com>
2017-08-04 22:31:21 +1200

    Background page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule
